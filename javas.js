b797d5dbeb63ff6c78bbd0173510437f2dd0a21b
abeerfedo <adamabeer44@gmail.com>
2025-07-14 21:44:26 +0300

    write the code for grid button

--- javas.js
+++ javas.js
@@ -1,13 +1,39 @@
+
+//variables :
 const container = document.querySelector("#container")
 const width = container.clientWidth
-console.log(width)
-const user = parseFloat("33")
+const grid = document.querySelector("#grid")
+let input = 0;
+let gridSize = 16
 
-for (let i = 0; i < user * user; i++) {
+//logic code
+gridmaker(gridSize)
 
-    let div1 = document.createElement("div")
-    div1.style.height = (width / user).toString() + "px"
-    div1.style.width = (width / user).toString() + "px"
-    container.appendChild(div1)
+grid.addEventListener("click", () => {
+    input = prompt("a grid between 1-100");
+    if (input > 0 && input < 100) {
+        gridSize = parseFloat(input)
+        gridmaker(gridSize)
+    }
+})
+
+function gridmaker(gridSize) {
+
+
+    resetContainer()
+    for (let i = 0; i < gridSize * gridSize; i++) {
+
+        let div1 = document.createElement("div")
+        div1.style.height = (width / gridSize).toString() + "px"
+        div1.style.width = (width / gridSize).toString() + "px"
+        container.appendChild(div1)
+    }
+
 }
 
+function resetContainer() {
+
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+}
