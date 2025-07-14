7a5e73e31164d96b3f7a4779f152bfcd2fc67588
abeerfedo <adamabeer44@gmail.com>
2025-07-14 18:41:24 +0300

    design the UI

--- javas.js
+++ javas.js
@@ -1,12 +1,13 @@
 const container = document.querySelector("#container")
-const width = container.clientWidth 
+const width = container.clientWidth
 console.log(width)
-const user = parseFloat("3")
-for( let i = 0 ; i < user*user ; i++){
+const user = parseFloat("33")
 
-    let div1= document.createElement("div")
-    div1.style.height= (width/ user).toString() + "px"
-    div1.style.width=(width/ user).toString() + "px"
+for (let i = 0; i < user * user; i++) {
+
+    let div1 = document.createElement("div")
+    div1.style.height = (width / user).toString() + "px"
+    div1.style.width = (width / user).toString() + "px"
     container.appendChild(div1)
 }
 
